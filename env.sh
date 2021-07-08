78fd6f97d9cf3f76ea19e3425fbc6db5c7736509
Bernd Haberstumpf <poldi@thatswing.de>
2021-07-08 20:41:21 +0200

    Replaced init.sh and env.sh with bashrc_local

--- env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export AWS_ACCOUNT=$(aws sts get-caller-identity|jq -r .Account)
-
-alias tf="terraform"
-alias tfi="terraform init"
-alias tfp="terraform plan -var account=${AWS_ACCOUNT} -input=false -out tfplan"
-alias tfa="terraform apply -auto-approve -input=false tfplan"
