a46650b4bae72525111cc9cf510c70d25736c908
Bernd Haberstumpf <poldi@thatswing.de>
2021-07-08 20:41:21 +0200

    Cleaned up frontend

--- chat-app/frontend/app.js
+++ chat-app/frontend/app.js
@@ -1,3 +1,5 @@
+var backendURL = 'wss://y0klom01si.execute-api.eu-west-1.amazonaws.com/Prod';
+
 var me = {};
 me.avatar = "./berni.gif";
 
@@ -11,79 +13,73 @@ function formatAMPM(date) {
     var ampm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0'+minutes : minutes;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
     var strTime = hours + ':' + minutes + ' ' + ampm;
     return strTime;
 }
 
-//-- No use time. It is a javaScript effect.
-function insertChat(who, text, time){
-    if (time === undefined){
-        time = 0;
-    }
+function insertChat(who, text) {
     var control = "";
     var date = formatAMPM(new Date());
 
-    if (who == "me"){
+    if (who == "me") {
         control = '<li style="width:100%">' +
-                        '<div class="msj macro">' +
-                        '<div class="avatar"><img class="img-circle" style="width:100%;" src="'+ me.avatar +'" /></div>' +
-                            '<div class="text text-l">' +
-                                '<p>'+ text +'</p>' +
-                                '<p><small>'+date+'</small></p>' +
-                            '</div>' +
-                        '</div>' +
-                    '</li>';
-    }else{
+            '<div class="msj macro">' +
+            '<div class="avatar"><img class="img-circle" style="width:100%;" src="' + me.avatar + '" /></div>' +
+            '<div class="text text-l">' +
+            '<p>' + text + '</p>' +
+            '<p><small>' + date + '</small></p>' +
+            '</div>' +
+            '</div>' +
+            '</li>';
+    } else {
         control = '<li style="width:100%;">' +
-                        '<div class="msj-rta macro">' +
-                            '<div class="text text-r">' +
-                                '<p>'+text+'</p>' +
-                                '<p><small>'+date+'</small></p>' +
-                            '</div>' +
-                        '<div class="avatar" style="padding:0px 0px 0px 10px !important"><img class="img-circle" style="width:100%;" src="'+you.avatar+'" /></div>' +
-                  '</li>';
+            '<div class="msj-rta macro">' +
+            '<div class="text text-r">' +
+            '<p>' + text + '</p>' +
+            '<p><small>' + date + '</small></p>' +
+            '</div>' +
+            '<div class="avatar" style="padding:0px 0px 0px 10px !important"><img class="img-circle" style="width:100%;" src="' + you.avatar + '" /></div>' +
+            '</li>';
     }
-    setTimeout(
-        function(){
-            $("ul").append(control).scrollTop($("ul").prop('scrollHeight'));
-        }, time);
-
+    $("ul").append(control).scrollTop($("ul").prop('scrollHeight'));
 }
 
-function resetChat(){
+function resetChat() {
     $("ul").empty();
 }
 
-$( document ).ready(function() {
-
-let client = new WebSocket('wss://y0klom01si.execute-api.eu-west-1.amazonaws.com/Prod', 'None');
-
-client.onmessage = function(e) {
-    insertChat("you",e.data);
-}
-
-$("#mytext").keydown( function(e){
-    if (e.which == 13){
-        var text = $(this).val();
-        if (text !== ""){
-            var msg = {
+function sendMessage(client,text) {
+    if (text !== "") {
+        let message = {
             action: "sendmessage",
             data: text
-            };
-            client.send(JSON.stringify(msg));
+        };
+        client.send(JSON.stringify(message));
+    }
+}
+
+$(document).ready(function () {
+
+    let client = new WebSocket(backendURL, 'None');
+
+    client.onmessage = function (e) {
+        insertChat("you", e.data);
+    }
+
+    $("#mytext").keydown(function (e) {
+        if (e.which == 13) {
+            let text = $(this).val();
+            sendMessage(client,text);
             $(this).val('');
         }
-    }
-});
+    });
 
-$('body > div > div > div:nth-child(2) > span').click(function(){
-    $("#mytext").trigger({type: 'keydown', which: 13, keyCode: 13});
+    $('#sendbutton').click(function () {
+        $("#mytext").trigger({ type: 'keydown', which: 13, keyCode: 13 });
+    })
+
+    resetChat();
+
+    insertChat(me, "Welcome!")
 })
-
-//-- Clear Chat
-resetChat();
-})
-
-
-//-- NOTE: No use time on insertChat.
